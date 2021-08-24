7756118b6c7fb7532b4e599811a1758c15da0158
Monika Geiger <geigermonika@hotmail.hu>
2021-08-24 12:31:00 +0200

    back to small canvas

--- js/background.js
+++ js/background.js
@@ -7,7 +7,7 @@ class Background {
         (this.x = 0);
         (this.y = 0);
         (this.width = 1500);
-        (this.height = 850);
+        (this.height = 700);
     }
     draw() {
         this.ctx.drawImage(bgImg, this.x, this.y, this.width, this.height)
